56ffd4046094f6c9aea47eb36e9c9cc01dd636ad
Terrance <terrance.rosas@protonmail.com>
2023-09-22 22:47:41 -0400

    add economiccalendar and marketoverview

--- project/project.py
+++ project/project.py
@@ -2,6 +2,7 @@
 from rxconfig import config
 
 import reflex as rx
+from reflex.components.component import NoSSRComponent
 
 docs_url = "https://reflex.dev/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
@@ -12,30 +13,33 @@ class State(rx.State):
 
     pass
 
+class EconomicCalendar(NoSSRComponent):
+    library = "react-ts-tradingview-widgets"
+    tag = "EconomicCalendar"
+
+    locale: rx.Var[str]
+
+    height: rx.Var[str]
+
+    width: rx.Var[str]
+
+class MarketOverview(NoSSRComponent):
+    library = "react-ts-tradingview-widgets"
+    tag = "MarketOverview"
+
+
 
 def index() -> rx.Component:
-    return rx.fragment(
-        rx.color_mode_button(rx.color_mode_icon(), float="right"),
-        rx.vstack(
-            rx.heading("Welcome to Reflex!", font_size="2em"),
-            rx.box("Get started by editing ", rx.code(filename, font_size="1em")),
-            rx.link(
-                "Check out our docs!",
-                href=docs_url,
-                border="0.1em solid",
-                padding="0.5em",
-                border_radius="0.5em",
-                _hover={
-                    "color": rx.color_mode_cond(
-                        light="rgb(107,99,246)",
-                        dark="rgb(179, 175, 255)",
-                    )
-                },
-            ),
-            spacing="1.5em",
-            font_size="2em",
-            padding_top="10%",
-        ),
+    return rx.hstack(
+        rx.container(
+        EconomicCalendar(
+            height="500",
+            width="400"
+            # locale="en"
+        )),
+        rx.container(
+        MarketOverview()
+        )
     )
 
 
