66116079139c46cad84634045c34eda3a3c9c5be
Terrance <91508152+Terrance808@users.noreply.github.com>
2023-11-24 18:20:48 -0500

    fix code

--- stock_backend/myapp/views.py
+++ stock_backend/myapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 import yfinance as yf
@@ -12,14 +13,28 @@ def get_stock_data(request): #, stockTickerInput):
     print("Request", request)
     # print("Data Pull", data)
 
-
+@csrf_exempt
 def post_stock_data(request):
     if request.method == "POST":
-        data = json.loads(request.body)
-        ticker_symbols = data.get('tickers', [])
-        stock_data = {}
-        for symbol in ticker_symbols:
-            ticker = yf.Ticker(symbol)
-            stock_data[symbol] = ticker.info
-        return JsonResponse(stock_data)
-    return JsonResponse({"error" : "Invalid request"}, status=400)
+        try:
+            data = json.loads(request.body)
+            ticker_symbol = data['stock_ticker']
+
+            if ticker_symbol:
+                stock = yf.Ticker(ticker_symbol)
+                try:
+                    history = stock.history(period="1d")
+                    closing_price = history['Close'].iloc[-1] if not history.empty else None
+                    print(type(closing_price))
+                    return JsonResponse({'status': 'success', 'ticker': ticker_symbol, 'current_price': closing_price})
+                except Exception as e:
+                    return JsonResponse({'status': 'error', 'message': f'Error fetching stock data: {e}'}, status=500)
+
+            else:
+                return JsonResponse({'status': 'error', 'message': 'Stock ticker symbol missing'}, status=400)
+        
+        except json.JSONDecodeError:
+            return JsonResponse({'status': 'error', 'message': 'Invalid JSON'}, status=400)
+
+    else:
+        return JsonResponse({'status': 'error', 'message': 'Invalid request'}, status=400)
